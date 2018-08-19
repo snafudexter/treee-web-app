75f417ab105768cf4d80833bd6aed30e9a6b77dc
Prabh Jot <prabh@live.in>
2018-08-19 17:48:26 +0530

    routing

--- src/App.js
+++ src/App.js
@@ -1,21 +1,14 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { Route, Switch, Redirect } from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
-}
+const App = () => (
+  <Switch>
+    {/* <Route exact path="/" render={() => (<Redirect to="/dashboard"/>)}/> */}
+    <Route exact path="/login" component={Login} />
+    <Route exact path="/signup" component={Signup} />
+  </Switch>
+);
 
 export default App;
